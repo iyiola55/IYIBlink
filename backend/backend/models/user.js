de76961d19588aea68ccbea799a63c39b31eecf9
iyiola55 <Iyiolasodiq2006@gmail.com>
2026-06-21 20:59:26 +0530

    Create User model with schema for MongoDB

---
+++ backend/backend/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema({
+
+name:{
+type:String,
+required:true
+},
+
+email:{
+type:String,
+required:true,
+unique:true
+},
+
+phone:String,
+
+
+password:{
+type:String,
+required:true
+},
+
+
+role:{
+type:String,
+default:"customer"
+}
+
+
+});
+
+
+module.exports = mongoose.model("User", UserSchema);
